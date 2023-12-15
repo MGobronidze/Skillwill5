dda6fc9f158f848fa5b3ef2866c78b70da565939
MGobronidze <90101429+MGobronidze@users.noreply.github.com>
2023-12-15 23:29:34 +0400

    add quiz

--- main.js
+++ main.js
@@ -42,17 +42,152 @@ bodyEl.append(divEl2);
 
 
 // დავალება 3
+// შევქმნათ ქვიზის სექცია
 let sectionEl = document.createElement("section");
 sectionEl.setAttribute("id","quiz-sec")
+
+// შევქმნათ სექციის სათური
 let quizH2El = document.createElement("h2");
 let quizTextEl = document.createTextNode("Quizz");
-let quizPEl1 = document.createElement("P");
-let quizTextElp1 = document.createTextNode("question 1");
 
-
+// დავამატოთ სექცია დოკუმენტში
 bodyEl.append(sectionEl);
-quizH2El.append(quizTextEl);
-quizPEl1.append(quizTextElp1)
-sectionEl.append(quizH2El, quizPEl1);
 
+// დავამატოთ სექციის სათაური
+quizH2El.append(quizTextEl);
+sectionEl.append(quizH2El);
+
+// შევქმნათ ქვიზის მასივი
+let quizArr = [ 
+    {
+        question: "What does HTML stand for?",
+        answer: [
+            {
+                ans: "Hyper Text Markup Language",
+                valid: "true"
+            },
+            {
+                ans: "Home Tool Markup Language",
+                valid: "false"
+            },
+            {
+                ans: "Hyperlinks and Text Markup Language",
+                valid: "false"
+            }
+        ]
+    },
+
+    {
+        question: "Who is making the Web standards?",
+        answer: [
+            {
+                ans: "Google",
+                valid: "false"
+            },
+            {
+                ans: "Mozilla",
+                valid: "false"
+            },
+            {
+                ans: "The World Wide Web Consortium",
+                valid: "true"
+            }
+        ]
+    },
+    {
+        question: "Choose the correct HTML element for the largest heading:",
+        answer: [
+            {
+                ans: "header",
+                valid: "false"
+            },
+            {
+                ans: "h1",
+                valid: "true"
+            },
+            {
+                ans: "head",
+                valid: "false"
+            }
+        ]
+    },
+    {
+        question: "What is the correct HTML element for inserting a line break?",
+        answer: [
+            {
+                ans: "<lb>",
+                valid: "false"
+            },
+            {
+                ans: "<break>",
+                valid: "fasle"
+            },
+            {
+                ans: "<br>",
+                valid: "true"
+            }
+        ]
+    },
+    {
+        question: "What is the correct HTML for adding a background color?",
+        answer: [
+            {
+                ans: "<body bg ='yellow'>",
+                valid: "false"
+            },
+            {
+                ans: "<background> yellow </background>",
+                valid: "fasle"
+            },
+            {
+                ans: "<background style ='background-color: yellow;'>",
+                valid: "true"
+            }
+        ]
+     }
+]
+
+// შევქმნათ და დავამატოთ ფორმა ქვიზის სექციაში
+let quizForm= document.createElement("form");
+sectionEl.append(quizForm);
+
+// ქვიზის მასივიდან დავამატოთ კითხვები და სავარაუდო პასუხები ქვიზის ფორმაში
+quizArr.forEach( el =>{
+    //შევქმნათ სექცია თითოეული კითხვისთვის    
+    const secEl =  document.createElement("section");
+    
+    // კითხვა
+    const questionEl = document.createElement("p");
+    questionEl.innerText= el.question;
+
+    // დავამატოთ თითოქული კითხვა ფომის სექციაშო
+    secEl.append(questionEl);
+    quizForm.append(secEl);
+        
+    // სავარაუდო პასუხი
+    const answ = [ans1, ans2, ans3] = [el.answer[0].ans, el.answer[1].ans, el.answer[2].ans];
+    const validity =[val1, val2, val3] =[el.answer[0].valid, el.answer[1].valid, el.answer[2].valid];
+    answ.forEach(ans => {
+
+        // თითოეული სავარაუდო პასუხისთვის შევქმნათ ინპუტის ველები ატრიბუტებით
+        const ansEl = document.createElement("input");
+        ansEl.setAttribute("id", ans);
+        ansEl.setAttribute("value", ans);
+        ansEl.setAttribute("name", "quiz");
+        ansEl.setAttribute("type", "radio");
+
+        // თითოეული სავარაუდო პასუხისთვის შევქმნათ ლეიბლები ატრიბუტებით
+        const ansLabelEl = document.createElement("label");
+        ansLabelEl.innerText=ans;
+        ansLabelEl.setAttribute("for", ans);
+
+        // ვადგენთ ინდექსს პასუხების მასივში
+        let index = answ.indexOf(ans);
+        ansLabelEl.classList.add(validity[index]);
+
+        // დავამატოთ სავარაუდო პასუხები სექციაში;
+        secEl.append(ansEl, ansLabelEl);
+    })
+}
+)
 
